db5b41b80b12e89819ba3043107cb263bae6388c
Walter Hoerman <whoerman@gmail.com>
2020-08-14 10:13:44 -0400

    Saving Basic Structure

---
+++ mainjs.js
@@ -0,0 +1,5 @@
+//defining the variable for animation end signal for animate.css
+var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+//defining the variables for animations to be used for animate.css
+var animationShake = "animated shake";
