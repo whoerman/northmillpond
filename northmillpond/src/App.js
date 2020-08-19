e6b85745b18ed74bfb694e846dfebba1e282cc75
Walter Hoerman <whoerman@gmail.com>
2020-08-19 12:41:59 -0400

    changing back from react to HTML

--- northmillpond/src/App.js
+++ northmillpond/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbartop from './components/Navbar/Navbartop';
 
 function App() {
   return (
     <div className="App">
+      <Navbartop/>
       <header className="App-header">
         <p>
           React App
