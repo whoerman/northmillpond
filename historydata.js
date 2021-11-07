65e456c4eb534e45c18b62c7cc9958d369493452
Walter Hoerman <whoerman@gmail.com>
2021-11-07 14:31:19 -0500

    make history Object

---
+++ historydata.js
@@ -0,0 +1,243 @@
+
+//setting the data for all the history cards
+let historyCardData = [{
+    description: '1631 pond view',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/birdseye1631.jpg',
+    imageAlt: '1631 image"',
+    cardTitle: "Bird's eye drawing from 1631",
+    cardText: "When the first Europeans arrived early in the 17th Century, North Mill Pond was a tidal inlet off the Piscataqua River with thickly wooded shores. The mighty old-growth trees of New Hampshire's forests were prized by the English Crown for their use as ships masts. Many of the first settlers came to New Hampshire, not in search of religious freedom but rather to seek their fortunes through trade with England, primarily in fish, fur, and timber.",
+    sourceInfo: 'C. S Gurney',
+},{
+    description: 'Jackson Home',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/JacksonHouse.jpg',
+    imageAlt: 'Jackson House',
+    cardTitle: "Jackson House 1664",
+    cardText: "In the great land division of 1660, land grants were finally given on the shores of the North Mill Pond. Richard Jackson, a cooper by trade, was given a 25 acre grant on the north side of the pond. The house he built in 1664 still sits on a hill above the pond on Northwest Street.",
+    sourceInfo: 'Historic New England and Richard Candee',
+},{
+    description: 'Waterhouse Ham House',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/waterhouseHam.jpg',
+    imageAlt: 'Waterhouse Ham House',
+    cardTitle: "Waterhouse Ham House",
+    cardText: 'The Timothy Waterhouse House, sometimes known as the <strong>Waterhouse-Ham House</strong>, is located at 273 Maplewood Avenue, on the northwest corner of Maplewood Avenue and Dearborn Street.  \
+    Timothy Waterhouse, a Portsmouth native employed as a tanner and shoemaker, was born in 1675. He built this home around 1700; however, its original location was about a half mile north on Freeman’s Point. At that time, the headland was named Ham’s Point for the original owner, William Ham. Timothy married Ruth Moses in January \
+    1700, and the Waterhouses had eleven children together. In 1725, there were nine rambunctious children living in this small home (six girls and three boys), from ages four to \
+    seventeen. There’s a local story about the time the kids were home alone, enjoying a rowdy evening in the absence of their parents. They could make all the noise they wanted, because the house was isolated, and there \
+    was a snowstorm blowing outside. Suddenly, there was a loud knocking at the door! All of the siblings abruptly hushed themselves: Margaret, Mary, Ruth, Joseph, Timothy, Sarah, Elizabeth, John, and Lydia. The bravest and \
+    oldest, Margaret, must have been apprehensive as she walked to the door and swung it open. In the dark doorway stood an apparition dressed all in white, with a black face. Thinking The Devil himself had come calling, \
+    Margaret immediately fainted. The visitor proved to be an African slave owned by another tanner, Nathaniel Jackson, who had been sent to retrieve Mr. Jackson’s shoes – possibly a new pair crafted by their father. The Timothy Waterhouse House moved to its present location on Maplewood Avenue around 1765 and remains a private residence.', 
+    sourceInfo: 'Walk Portsmouth',
+  },{
+    description: 'Miller development',
+    cardtpe: 'blockQuote',
+    imageSource: '',
+    imageAlt: '',
+    cardTitle: "",
+    cardText: 'In 1803, local Joiner John Miller, "an enterprising, capable architect", laid out the development \
+    encompassing Sparhawk to Woodbury. When he died in 1813, he had lumber waiting on the southern shore where \
+    he planned on building a bridge from the Rock Pasture across the pond to Sparhawk. The bridge was never \
+    built.',
+    sourceInfo: 'Richard Candee',
+  },{
+    description: '',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/hosieryfactory.jpg',
+    imageAlt: 'Hosiery factory',
+    cardTitle: "The Stocking and Yarn Factory",
+    cardText: 'The only water powered mill in Portsmouth was operated on the west end of North Mill Pond. \
+    <strong>The Stocking and Yarn Factory</strong> was located on Islington Creek (today Hodgdon Brook at Bartlett \
+    & Cate streets) in 1834. When that factory burned five years later, it was replaced under new owners as part \
+    of <strong>the Rockingham Steam Factory</strong>. Some 220 men, women, girls, and boys eventually found \
+    employment at this company, working directly at the factory or from their home as “seamers and finishers.” \
+    Up to 1,200 pairs of stockings could be manufactured per week. ',
+    sourceInfo: '',
+  },{
+    description: '1840 Pond fill in',
+    cardtpe: 'blockQuote',
+    imageSource: '',
+    imageAlt: '',
+    cardTitle: "",
+    cardText: "Around 1840, to accomodate several railroads needed to connect Portsmouth to Massachusetts and Boston, the \
+    south side of North Mill Pond was partially filled in, and an industrial neighboorhood evolved around one large \
+    cotton textile mill and several other small industries.",
+    sourceInfo: '',
+  },{
+    description: "1850 Rayne's Shipyard",
+    cardtpe: 'pictureCard',
+    imageSource: 'history/1850Causeway.jpg',
+    imageAlt: '1850 Map',
+    cardTitle: "1850 Map - Rayne's Shipyard",
+    cardText: "In 1850, the <strong>Rayne's Shipyard</strong> dominated the beginning of North Mill Pond. \
+    The Causeway across the Pond was then call Elm Street (now Maplewood).",
+    sourceInfo: '1850 Map Maine Memory Network',
+  },{
+    description: 'Witch of the Wave',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/Witchofthewave.jpg',
+    imageAlt: 'Witch of the wave',
+    cardTitle: "Witch of the Wave",
+    cardText: "George Raynes purchased the property called the <strong>Rayne's Shipyard</strong> in 1832 \
+    and turned the shipyard into a booming business. From 1832-1855, he constructed from sixty to seventy ships of all sizes. \
+    Some of the largest and fastest clipper ships that ever sailed were constructed at the Raynes shipyard, \
+    including the Sea-Serpent (1850), the Witch-of-the-Wave (1851), the Webster (1853); and the Sierra Nevada and \
+    Coeur de Lion (1854).",
+    sourceInfo: 'from Walk Portsmouth',
+  },{
+    description: 'Portsmouth Steam Company',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/1850SouthShore.jpg',
+    imageAlt: 'Sparhawk Map',
+    cardTitle: "1850 Map - South Shore",
+    cardText: "In 1850, the Portsmouth Steam Company manufacturing cotton textiles and the Sagamore Mill \
+    occupied the southern shore of North Mill Pond. ",
+    sourceInfo: '1850 Map Maine Memory Network',
+  },{
+    description: 'Kearsarge mill',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/1877MapCloseupKeasargeMills.jpg',
+    imageAlt: 'Kearsarge Mill',
+    cardTitle: "Keasarge Mill 1877",
+    cardText: "Built in 1850, the six-story <strong>Portsmouth Steam Company</strong> was a huge presence \
+    on the south shore of North Mill Pond. Over 400 people were employed in the manufacture of cotton fabrics. In 1866, it was \
+    reorganized into the <strong>Kearsarge Mills</strong>. On Dec. 4, 1880, the mill was ravaged by a fire \
+    described in Portsmouth Fire Department historical records 'as the worst public calamity since the Great Fire of 1813.' The fire \
+    destroyed the building’s top five floors, leaving only the ground level floor. The owners decided not to \
+    rebuild, and the empty ruin was sold.",
+    sourceInfo: '',
+  },{
+    description: '1850 Christian Shores',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/1850ChristianShores.jpg',
+    imageAlt: 'Christian Shores',
+    cardTitle: "1850 Map - Christian Shores",
+    cardText: "In 1850, there were few houses and streets on the north side of North Mill Pond. Dennett \
+    Street ended at the fork of Myrtle and Woodbury (that section is now Thornton Street). ",
+    sourceInfo: '1850 Map Maine Memory Network',
+  },{
+    description: '1850 North Creek',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/1850Sparhawk.jpg',
+    imageAlt: 'Sparhawk map',
+    cardTitle: "1850 Map - North Creek Area",
+    cardText: "As you can see from this Map of 1850, the northern Creek area was still largely vacant, \
+    with nothing between Sparhawk and Bartlett Streets. The Rockingham Steam Factory is at the bottom. ",
+    sourceInfo: '1850 Map Maine Memory Network',
+  },{
+    description: 'West end Brewery',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/Breweries.jpg',
+    imageAlt: 'Breweries',
+    cardTitle: "Brewery Map - West End",
+    cardText: "After the Civil War, thanks to entrepreneurs like Frank Jones and Herman Eldredge, Portsmouth \
+    became synonymous with beer, delivering the nation’s largest output of beer (100,000 gallons per year) across \
+    the country. Beer production dominated the West end of North Mill Pond.",
+    sourceInfo: '1892 Map by D. H. Hurd and Co.',
+  },{
+    description: 'Eldridge Brewery',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/Eldridge.jpg',
+    imageAlt: 'Eldridge Brewery',
+    cardTitle: "Eldridge Brewery",
+    cardText: "The Eldredge family began brewing in the former Portsmouth Stocking Factory on the North \
+    Mill Pond West End around 1858, and the M. Fisher Brewing Company soon opened with Marcellus Eldredge in \
+    charge. His father, Herman Eldredge, bought the firm in 1870 and renamed it the H. Eldredge and Son Company. \
+    They shortened the name to the <strong>Eldredge Brewing Company</strong> in 1875. Eldredge Brewing Company produced 30,000 \
+    barrels of beer in 1870 and grew to almost 93,000 barrels of beer by 1892. It was the second largest brewery \
+    in Portsmouth, after the Frank Jones Brewery. <em>(Today, the original ice vault for beer storage from the \
+      Eldredge Brewing Company is now the office building called Eldredge Park at One Cate Street)",
+    sourceInfo: 'Walk Portsmouth',
+  },{
+    description: '1877 Creek Map',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/1877MapCreek.jpg',
+    imageAlt: '1877 Creek map',
+    cardTitle: "1877 Street Map",
+    cardText: "By 1877, most of the streets of the Creek Area of North Mill Pond had been laid out.",
+    sourceInfo: '',
+  },{
+    description: 'Railroad Station',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/B&MStation.jpg',
+    imageAlt: 'railroad station',
+    cardTitle: "B & M Porstouth Rail Station",
+    cardText: "Located on the south side of North Mill Pond The <strong>Boston & Maine railroad \
+    station</strong> in Portsmouth, N.H. was once the end of the line for passengers coming from Boston.",
+    sourceInfo: '',
+  },{
+    description: '1894 train crash',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/crash1894.jpg',
+    imageAlt: '1894 train crash',
+    cardTitle: "B & M Crash 1894",
+    cardText: "This crash knocked the engine directly onto its side in Portsmouth, N.H. station on August \
+    12, 1894.",
+    sourceInfo: '',
+  },{
+    description: 'Ruins of Mill 1898',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/898ruinsofmill.jpg',
+    imageAlt: '1898 mill ruins',
+    cardTitle: "Ruins of Mill 1898",
+    cardText: "The last mill on the Maplewood causeway ceased operation in 1888, more than 200 years after \
+    the first mill built by John Cutt in 1659.",
+    sourceInfo: 'Portsmouth Atheneum',
+  },{
+    description: 'North Mill Pond Bridge 1902',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/1902closeuptrolley.jpg',
+    imageAlt: 'North Mill Pond Bridge 1902',
+    cardTitle: "North Mill Pond Bridge 1902",
+    cardText: "From 1899 to 1925, the <strong>Portsmouth Electric Railway</strong> operated the local \
+    electric trolleys, with a track that went right over the Maplewood Causeway. It was even possible to take a \
+    trolley all the way to Concord, but it took 7 hours! Sadly, the trolleys were discontinued in 1925.",
+    sourceInfo: '',
+  },{
+    description: 'B & M Crash 1909',
+    cardtpe: 'pictureCard',
+    imageSource: 'history/crash1909.jpg',
+    imageAlt: 'B & M Crash 1909',
+    cardTitle: "B & M Crash 1909",
+    cardText: "A crash at the Portsmouth, N.H. station made headlines in May 1909 when one B&M passenger \
+    train rear-ended another at noon. Seven people were injured and fireman on the yard engine was killed.",
+    sourceInfo: '',
+  },{
+    description: 'Route 1 1940',
+    cardtpe: 'blockQuote',
+    imageSource: '',
+    imageAlt: '',
+    cardTitle: "Route 1 1940",
+    cardText: "<img src='./assets/images/history/Route1.jpg'></img> The US 1 Bypass was built in 1940, part of a major New Deal \
+    project to move U.S. Route 1 traffic away from the congested \
+    streets of downtown Portsmouth. The Bypass created a second Piscataqua River crossing into Maine via the \
+    Interstate (Sarah M. Long) Bridge. The highway's wide divided lanes and grade separations were the first in \
+    the state, earning it the title 'New Hampshire's Most Modern Highway.' Today, the U.S. Route 1 Bypass is one \
+    of the oldest signed bypasses in the country's numbered route system.",
+    sourceInfo: '',
+  },{
+    description: 'Route 1 1940',
+    cardtpe: 'blockQuote',
+    imageSource: '',
+    imageAlt: '',
+    cardTitle: "Route 1 1940",
+    cardText: "<img src='./assets/images/history/Route1.jpg'></img> The US 1 Bypass was built in 1940, part of a major New Deal \
+    project to move U.S. Route 1 traffic away from the congested \
+    streets of downtown Portsmouth. The Bypass created a second Piscataqua River crossing into Maine via the \
+    Interstate (Sarah M. Long) Bridge. The highway's wide divided lanes and grade separations were the first in \
+    the state, earning it the title 'New Hampshire's Most Modern Highway.' Today, the U.S. Route 1 Bypass is one \
+    of the oldest signed bypasses in the country's numbered route system.",
+    sourceInfo: '',
+  },
+];
+
+
+//setting the data for all the history resourcecards
+let historyResourcesData = [{
+  resourceName: 'Richard Candee',
+  resourceInfo: '<strong>Richard Candee</strong> has been president of the Portsmouth Atheneum and the Portsmouth \
+  Historical Society, was the author of "Building Portsmouth", and is professor emeritus of American and New \
+  England Studies at Boston University.',
+},
+];
