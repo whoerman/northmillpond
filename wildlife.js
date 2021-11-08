b0da7b0dd41d5a0901d4a5628c902262c8dc76b4
Walter Hoerman <whoerman@gmail.com>
2021-11-08 15:40:52 -0500

    bird buttons

--- wildlife.js
+++ wildlife.js
@@ -1,17 +1,62 @@
+
+
+
+
+
+
+//formatting the buttons and putting them on page
 duckButton =
-'<button type="button" id="duckButton" class="btn btn-success">Duck</button>';
-//placing the duck card in the content div
+'<button type="button" id="duckButton" class="btn btn-secondary btn-large btn-block" style = "font-size:1.5rem;">Is it swimming like a duck?</button>';
 document.getElementById("wildlifediv").innerHTML += duckButton;
-//adding the event listener to the button on the intro page
-document.getElementById("duckButton").addEventListener("click", function () {
-console.log("quack");
-});
 
 waderButton =
-'<button type="button" id="waderButton" class="btn btn-primary">Wader</button>';
-//placing the wader button in the wildlife div
+'<button type="button" id="waderButton" class="btn btn-primary btn-large btn-block" style = "font-size:1.5rem;">Is it walking around on long legs?</button>';
 document.getElementById("wildlifediv").innerHTML += waderButton;
-//adding the event listener to the wader button on the page
-document.getElementById("waderButton").addEventListener("click", function () {
-console.log("that is a wader!");
-});
+
+gullButton =
+'<button type="button" id="gullButton" class="btn btn-secondary btn-large btn-block" style = "font-size:1.5rem;">Is it flying around like a seagull?</button>';
+document.getElementById("wildlifediv").innerHTML += gullButton;
+
+hawkButton =
+'<button type="button" id="hawkButton" class="btn btn-primary btn-large btn-block" style = "font-size:1.5rem;">Is it a soaring like a hawk?</button>';
+document.getElementById("wildlifediv").innerHTML += hawkButton;
+
+smallbirdButton =
+'<button type="button" id="smallbirdButton" class="btn btn-secondary btn-large btn-block" style = "font-size:1.5rem;">Is it a small bird on a branch?</button>';
+document.getElementById("wildlifediv").innerHTML += smallbirdButton;
+
+//adding the event listeners to the buttons on the bird page
+function addEvents() {
+    document.getElementById("duckButton").addEventListener("click", function () {
+        duckClick()});
+    document.getElementById("waderButton").addEventListener("click", function () {
+        waderClick()});
+    document.getElementById("gullButton").addEventListener("click", function () {
+        gullClick()});
+    document.getElementById("hawkButton").addEventListener("click", function () {
+        hawkClick()});
+    document.getElementById("smallbirdButton").addEventListener("click", function () {
+        smallbirdClick()});
+};
+
+function duckClick() {
+    console.log("quack")
+}
+
+function waderClick() {
+    console.log("look at me walking!")
+}
+
+function gullClick() {
+    console.log("give me a french fry!")
+}
+
+function hawkClick() {
+    console.log("Look out below!")
+}
+
+function smallbirdClick() {
+    console.log("Fill the bird feeder!")
+}
+
+addEvents();
